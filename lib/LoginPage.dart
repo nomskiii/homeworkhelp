9c0fdf959d5042a3e437a5dca6f7851446ce2490
alexaney <aneya@bxscience.edu>
2022-05-19 08:56:06 -0400

    commit

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -15,33 +15,15 @@ class LoginPage extends StatelessWidget {
     return MaterialApp(
       title: 'Login',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Login'),
+      home: const MyHomePage(title: 'Login Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -85,18 +67,28 @@ class _MyHomePageState extends State<MyHomePage> {
               controller: myController2,
             ),
             SizedBox(height: 15),
-            FloatingActionButton(
-              onPressed: ,
-              child: const Text("Login",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+            SizedBox(
+              height:60,
+              width:100,
+              child: FloatingActionButton(
+                shape: BeveledRectangleBorder(
+                    borderRadius: BorderRadius.zero),
+                onPressed: () {
+                  email = myController.text;
+                  password = myController2.text;
+                },
+                child: const Text("Login",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+              ),
             ),
-            RaisedButton(
+
+            ElevatedButton(
               child: Text("Don't have an account!? Click to register homie"),
               onPressed: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => const RegisterPage())
-                ),
+                );
               },
             )
           ],
--- lib/RegisterPage.dart
+++ lib/RegisterPage.dart
@@ -1,9 +1,114 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'LoginPage.dart';
 
-
-void main() {
-  runApp(const RegisterPage());
-}
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Register',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Register Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late String name;
+  late String email;
+  late String  password;
+
+  Widget build(BuildContext context) {
+    final myController = TextEditingController();
+    final myController2 = TextEditingController();
+    final myController3 = TextEditingController();
+    final myController4 = TextEditingController();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Enter your full name',
+                ),
+                controller: myController,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Enter your email',
+                ),
+                controller: myController2,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Enter your password',
+                ),
+                controller: myController3,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Confirm password',
+                ),
+                controller: myController4,
+              ),
+              SizedBox(height: 15),
+              SizedBox(
+                height:60,
+                width:150,
+                child: FloatingActionButton(
+                  shape: BeveledRectangleBorder(
+                      borderRadius: BorderRadius.zero),
+                  onPressed: () {
+                    name = myController.text;
+                    email = myController2.text;
+                    password = myController3.text;
+                    if (myController4.text == password) {
+                      Navigator.pop(context);
+                    } else {
+                      Fluttertoast.showToast(
+                      msg: "Please confirm your password to match the password entered",
+    toastLength: Toast.LENGTH_SHORT,
+    gravity: ToastGravity.CENTER,
+    timeInSecForIosWeb: 5,
+    backgroundColor: Colors.red,
+    textColor: Colors.white,
+    fontSize: 16.0
+    );
+                    }
+                  }
+
+
+
+                  child: const Text("Register",
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+                ),
+              )
+
+            ],
+        )
+      )
+    );
+  }
+}
