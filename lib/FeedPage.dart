065885e4f91c464a08ea0ac7697ec8ea45e017ab
nomskiii <noamcanter@gmail.com>
2022-06-13 09:33:55 -0400

    fuck noam i actually hate this kid so much

--- lib/FeedPage.dart
+++ lib/FeedPage.dart
@@ -1,10 +1,14 @@
+
+import 'package:event/event.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:homeworkhelp/UploadPage.dart';
 
 class FeedPage extends StatefulWidget {
-  final String email;
-  const FeedPage({Key? key, required this.email}) : super(key: key);
+  String email;
+  String name = '';
+  FeedPage({Key? key, required this.email}) : super(key: key);
 
   @override
   State<FeedPage> createState() => _FeedPageState();
@@ -14,13 +18,37 @@ class FeedPage extends StatefulWidget {
 
 
 class _FeedPageState extends State<FeedPage> {
-  
+
+  DatabaseReference _database = FirebaseDatabase.instance.ref();
+  String _performSingleFetch() {
+    _database.child('users').get().then((snapshot) {
+      if (snapshot != null) {
+        final users = Map<String, dynamic>.from((snapshot.value! as Event).snapshot.value;
+
+        users.forEach((key, value) {
+          final nextUser = Map<String, dynamic>.from(value);
+          if(nextUser['Email'] == widget.email) {
+            return nextUser['Name'];
+      }
+      });
+      }
+    });
+    return 'notThere';
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Classes"),
       ),
       body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Email: $widget.email'),
+            Text('Name: $_performSingleFetch()')
+          ],
+        )
         //child: ListView.builder(
         //    itemCount:
         //    itemBuilder: (BuildContext context, int index) {
--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -12,7 +12,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
